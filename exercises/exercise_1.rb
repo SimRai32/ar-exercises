676d90bde0e0503ab5a6e1c426945d13f4ebf17f
Simran Kalirai <kaliraisimran@gmail.com>
2022-10-17 16:54:44 -0700

    exercise 1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+store1Data = {"name" => "Burnaby", "annual_revenue" => 300000, "mens_apparel" => true, "womens_apparel" => true}
+store1 = Store.create(store1Data)
+store2Data = {"name" => "Richmond", "annual_revenue" => 1260000, "mens_apparel" => false, "womens_apparel" => true}
+store2 = Store.create(store2Data)
+store3Data = {"name" => "Gastown", "annual_revenue" => 190000, "mens_apparel" => true, "womens_apparel" => false}
+store3 = Store.create(store3Data)
+puts"#{Store.count}"
